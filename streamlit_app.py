b6167fbba03f481b0e4d444085daaa35464d77c6
shivamjawahar <shivamjawahar@gmail.com>
2024-07-26 22:38:11 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,4 @@
-#import streamlit as st
-
-#st.write('hai')
-
-import pandas as pd
-import pandas_profiling
 import streamlit as st
 
-from streamlit_pandas_profiling import st_profile_report
+st.write('hai')
 
-df = pd.read_csv("iris.csv")
-pr = df.profile_report()
-
-st_profile_report(pr)
