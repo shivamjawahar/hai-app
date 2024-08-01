eae38ce8baaf8ad68a55bc3bde0c8bd731912829
shivamjawahar <shivamjawahar@gmail.com>
2024-08-01 23:22:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('🤖 Machine Learning App')
+
+st.info('This is app builds a machine learning model!')
 
