7abe53fbc6f3367cdc3bfc49617f1d8dc9371044
shivamjawahar <shivamjawahar@gmail.com>
2024-08-01 23:28:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+
