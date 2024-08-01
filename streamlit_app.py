7023657adad4945aa54e16a0fd8474970729c732
shivamjawahar <shivamjawahar@gmail.com>
2024-08-01 23:45:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,9 @@ st.info('IRIS machine learning model!')
 with st.expander('IRIS values'):
   df = pd.read_csv('iris.csv')
   df
+
+  st.write('**X**')
+  X_raw = df.drop('variety', axis=1)
+  X_raw
   
   
